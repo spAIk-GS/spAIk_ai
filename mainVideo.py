5db1f19b49fbc8ce9edca9525ab97405858d0e62
sh <116047147+vmfpel456@users.noreply.github.com>
2025-08-19 17:23:32 +0900

    Update mainVideo.py

--- mainVideo.py
+++ mainVideo.py
@@ -281,7 +281,7 @@ def run(video_path):
             pass
 
 if __name__ == "__main__":
-    video_path = "C:/Users/Dell/Desktop/testK.mp4"
+        video_path = str(input("주소를 입력하세요: "))
     with open("data.json", "w", encoding="utf-8") as f:
         data = run(video_path)
         json.dump(data, f, ensure_ascii=False, indent=4)
